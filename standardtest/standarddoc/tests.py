35f30d9c55ccf8eb160f63f84bc74384f2d98f5d
allthatilk <allthatilk@users.noreply.github.com>
2018-02-23 18:58:30 +0000

    WIP add tests for XSLT transformations

--- standardtest/standarddoc/tests.py
+++ standardtest/standarddoc/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+# from django.test import TestCase
+# import pytest
+# from .activity_schema_full import FULL_ACTIVITY_SCHEMA_STRING
+from standarddoc import views
 
-# Create your tests here.
+
+XSL_FILE = 'iati-activities.xsl'
+
+
+def test_etree_to_HTML_transformation():
+    """Check that an HTML file is created from an IATI XSD schema."""
+    activity_schema_tree = create_full_activity_schema_etree()
+    transform_XSD_to_HTML(activity_schema_tree)
+    assert os.path.is_file('iati-activities.html')
--- standardtest/standarddoc/views.py
+++ standardtest/standarddoc/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+import iati
+from lxml import etree
 
-# Create your views here.
+
+XSL_FILE = 'iati-activities.xsl'
+
+
+def create_full_activity_schema_etree():
+    """Create an IATI schema with all elements and attributes from the common schema included."""
+    activity_schema = iati.default.activity_schema('2.02')
+    activity_schema_tree = activity_schema.flatten_includes(activity_schema._schema_base_tree)
+    return activity_schema_tree
+
+def transform_XSD_to_HTML(activity_schema_tree):
+    # """Create an HTML file from an IATI XSD schema."""
+    # activity_schema_full = activity_schema_tree
+    # xslt = etree.parse(XSL_FILE)
+    # transform = etree.XSLT(xslt)
+    # html_file = transform(activity_schema_full)
+    # with open('iati-activities.html', 'w') as output_file:
+    #     output_file.write(html_file)
