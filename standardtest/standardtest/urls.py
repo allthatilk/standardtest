099756c13d0ec56b12fc34cde004ae28e3830af5
allthatilk <allthatilk@users.noreply.github.com>
2018-03-14 17:30:08 +0000

    update urls.py for django 2.0

--- standardtest/standardtest/urls.py
+++ standardtest/standardtest/urls.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import include, path, re_path
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
@@ -10,19 +10,21 @@ from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
 from standarddoc import views as standarddoc_views
+
+print(admin.site.urls)
 urlpatterns = [
-    url(r'^django-admin/', include(admin.site.urls)),
+    path('django-admin/', admin.site.urls),
 
-    url(r'^admin/', include(wagtailadmin_urls)),
-    url(r'^documents/', include(wagtaildocs_urls)),
+    path('admin/', include(wagtailadmin_urls)),
+    path('documents/', include(wagtaildocs_urls)),
 
-    url(r'^search/$', search_views.search, name='search'),
+    re_path(r'^search/$', search_views.search, name='search'),
 
-    url(r'^iati-activities/', standarddoc_views.iati_activities),
+    path('iati-activities/', standarddoc_views.iati_activities),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    url(r'', include(wagtail_urls)),
+    path('', include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
     #    url(r'^pages/', include(wagtail_urls)),
