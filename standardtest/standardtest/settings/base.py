584d7a6f18e26d0b5a9cca61c63eb56cf433a521
allthatilk <allthatilk@users.noreply.github.com>
2018-03-14 12:52:46 +0000

    edit static file dirs WIP

--- standardtest/standardtest/settings/base.py
+++ standardtest/standardtest/settings/base.py
@@ -129,9 +129,11 @@ STATICFILES_FINDERS = [
 
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(BASE_DIR, 'standarddoc', 'static'),
+
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
