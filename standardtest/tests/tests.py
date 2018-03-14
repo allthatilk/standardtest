b4285f77bd652631079e26b4ed76bd39a8ed88d9
allthatilk <allthatilk@users.noreply.github.com>
2018-03-14 11:45:01 +0000

    update tests to reflect changes to standarddoc app

--- standardtest/tests/tests.py
+++ standardtest/tests/tests.py
@@ -1,11 +1,11 @@
 """A module containing tests."""
 import os
-from standarddoc import views
+from standarddoc import models
 
 
 def test_HTML_file_is_created():
     """Check that an HTML file is created from an IATI XSD schema."""
-    xslt_transformation = views.XSLTTransformation()
+    xslt_transformation = models.XSLTTransformation()
     xslt_transformation.transform_XSD_to_HTML()
     path = 'standardtest/templates/iati-activities.html'
     assert os.path.isfile(path)
