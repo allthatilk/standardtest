2d2f91618c74368bf108f3b12481ba294f7ad16d
allthatilk <allthatilk@users.noreply.github.com>
2018-03-14 11:27:59 +0000

    add standardtest to installed apps and reorganise standard docs WIP

--- standardtest/standarddoc/views.py
+++ standardtest/standarddoc/views.py
@@ -1,37 +1,5 @@
 from django.shortcuts import render
-import iati
-from lxml import etree
-
-
-class XSLTTransformation(object):
-
-    XSL_FILE = 'standarddoc/iati_activities.xsl'
-
-    def create_full_activity_schema_etree(self):
-        """Create an IATI schema with arelevant content from the common schema included."""
-        activity_schema = iati.default.activity_schema('2.02')
-        activity_schema_tree = activity_schema.flatten_includes(activity_schema._schema_base_tree)
-        return activity_schema_tree
-
-    def parse_XSL_template(self):
-        """Turn XSL document into an XSLT object."""
-        return etree.parse(self.XSL_FILE)
-
-    def transform_XSD_to_HTML(self):
-        """Create an HTML file from an IATI XSD schema."""
-        activity_schema_full = self.create_full_activity_schema_etree()
-        transform = etree.XSLT(self.parse_XSL_template())
-        html_tree = transform(activity_schema_full)
-        # The write method is used as the write_output method that uses the xsl:output tag doesn't provide correct indenting or DOCTYPE format.
-        html_tree.write(
-            'standardtest/templates/iati-activities.html',
-            doctype='<!DOCTYPE html>',
-            encoding='UTF-8',
-            pretty_print=True
-        )
-        # return(html_tree)
-
 
 def iati_activities(request):
     """Render the iati_activities page."""
-    return render(request, 'standarddoc/iati-activities.html', {})
+    return render(request, 'iati-activities.html', {})
--- standardtest/standardtest/settings/base.py
+++ standardtest/standardtest/settings/base.py
@@ -18,7 +18,6 @@ import os
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -29,6 +28,7 @@ INSTALLED_APPS = [
     'home',
     'search',
     'standarddoc',
+    'standardtest',
 
     'wagtail.wagtailforms',
     'wagtail.wagtailredirects',
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
